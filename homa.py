4efcfcd5dd5c25939c05317992149204824d6392
Apaosha <alirezaa4085@gmail.com>
2023-07-23 23:30:31 +0330

    Display message when pause,resume,screenshot.

--- homa.py
+++ homa.py
@@ -11,6 +11,8 @@ if not cap.isOpened():
 pause = False
 wait_time = 25
 playback_speed = 1.0
+message = ""
+message_duration = 0
 while cap.isOpened():
     if not pause:
         ret, frame = cap.read()
@@ -24,8 +26,19 @@ while cap.isOpened():
         cv2.rectangle(frame, (0, 0), (text_width + 10, text_height + 10), (0, 0, 0), -1)
         cv2.putText(frame, text, (5, text_height + 5), font, 1, (255, 255, 255), 2)
 
+        # Display message on the top right corner of the frame
+        if message_duration > 0:
+            text_size, _ = cv2.getTextSize(message, font, 1, 2)
+            text_width, text_height = text_size
+            x = frame.shape[1] - text_width - 15
+            y = text_height + 5
+            cv2.rectangle(frame, (x - 5, 0), (x + text_width + 5, y + 5), (0, 0, 0), -1)
+            cv2.putText(frame, message, (x, y), font, 1, (255, 255, 255), 2)
+            message_duration -= wait_time
+
         # Display the read frame
         cv2.imshow('frame', frame)
+        cv2.setWindowTitle('frame', 'Homa')
 
         # Press 'q' to exit
         key = cv2.waitKey(wait_time)
@@ -33,14 +46,22 @@ while cap.isOpened():
             break
         elif key & 0xFF == ord(' '):
             pause = not pause
+            message = "Pause" if pause else "Play"
+            message_duration = 1000
         elif key & 0xFF == ord('s'):
             cv2.imwrite('screenshot.png', frame)
+            message = "Screenshot saved"
+            message_duration = 1000
         elif key & 0xFF == ord('+'):
             wait_time = max(1, wait_time - 5)
             playback_speed = 25.0 / wait_time
+            message = f"Speed: {playback_speed:.1f}x"
+            message_duration = 1000
         elif key & 0xFF == ord('-'):
             wait_time += 5
             playback_speed = 25.0 / wait_time
+            message = f"Speed: {playback_speed:.1f}x"
+            message_duration = 1000
     else:
         break
 
